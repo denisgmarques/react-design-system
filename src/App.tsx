63136d0fcaae82b702257650c8ad624b9b021052
Denis Giovan Marques <denis.marques@icolabora.com.br>
2022-10-13 21:17:29 -0300

    Components with storybook

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+    <h1 className='text-2xl font-bold'>Hello World</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
+        Enviar
+      </button>
+    </>
+  )
+}
+
+export default App
